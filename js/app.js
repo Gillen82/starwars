919d077b100a9d45f31d65ba18e31700076435f0
Stevie Gillen <stevie_gillen@hotmail.com>
2022-03-04 13:31:17 +0000

    Fixed JS to accept limited number of characters. Added relevant images to match characters

--- js/app.js
+++ js/app.js
@@ -3,20 +3,25 @@
 const character = document.querySelector('.character');
 const planet = document.querySelector('.planet');
 const select = document.querySelector('#char-select');
+const charImg = document.querySelector('#char-img');
 
 select.addEventListener('click', () => {
-	let randNumber = Math.floor(Math.random() * 82) + 1;
+	let randNumber = Math.floor(Math.random() * 12) + 1;
+	if (randNumber === 17) {
+		randNumber = 16;
+	}
 	getStarWars(randNumber);
 });
 
 const getStarWars = async (id) => {
 	try {
 		const res = await axios.get(`https://swapi.dev/api/people/${id}`);
-		const name = res.data.name;
-		character.innerText = name;
+		const name = await res.data.name;
 		const world = await axios.get(res.data.homeworld);
-		const result = world.data.name;
+		const result = await world.data.name;
+		character.innerText = name;
 		planet.innerText = result;
+		charImg.src = `imgs/${id}.jpg`;
 	} catch (e) {
 		console.log(e);
 	}
